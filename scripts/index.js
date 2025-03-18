aee0d6de9f9ade3d2a4a39626dddbbf0756e8acc
kostlv <flowery_heaving0c@icloud.com>
2025-03-18 18:55:26 +0100

    feat: add places card on page load and delete cards on button click

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+const cardElement = cardTemplate.querySelector(".places__item.card");
 
-// @todo: DOM узлы
+function addCard(title, imagelink, cardDeleteFunction) {
+  const cardInstance = cardElement.cloneNode(true);
+  const deleteButton = cardInstance.querySelector(".card__delete-button");
 
-// @todo: Функция создания карточки
+  cardInstance.querySelector(".card__image").src = imagelink;
+  cardInstance.querySelector(".card__title").textContent = title;
 
-// @todo: Функция удаления карточки
+  deleteButton.addEventListener("click", () => {
+    cardDeleteFunction(cardInstance);
+  });
 
-// @todo: Вывести карточки на страницу
+  placesList.append(cardInstance);
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+initialCards.forEach((card) => {
+  addCard(card.name, card.link, deleteCard);
+});
